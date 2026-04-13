2e89095b91c51a4ee1c6a8114fc10b069300786a
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-04-13 22:01:50 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+  return {"message" : "Hellllo"}
+
+@app.get("/skill")
+def skill(name , str):
+  return ("skill" : name)
+ 
